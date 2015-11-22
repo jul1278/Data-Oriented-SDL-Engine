9702d525a52c48c0f6d7bb0e8a1360ad4d568ba5
jul1278 <jul1278@gmail.com>
2015-11-22 21:44:33 +1000

    Added BaseComponent

---
+++ Headers/BaseComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by Julian  on 17/11/15.
+//
+
+#ifndef SDL_RTS_ICOMPONENT_H
+#define SDL_RTS_ICOMPONENT_H
+
+// NOTE: BaseComponent not IComponent
+
+struct BaseComponent
+{
+    int id;
+};
+
+
+#endif //SDL_RTS_ICOMPONENT_H
