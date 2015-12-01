f6bebbaa4030e3d47ea35774348980454c05774b
Julian <jul1278@gmail.com>
2015-12-01 17:38:03 +1000

    resharper fixes

--- Headers/GraphicsResources/CircleFIllGraphicsResource.h
+++ Headers/GraphicsResources/CircleFIllGraphicsResource.h
@@ -14,26 +14,16 @@ class CircleFillGraphicsResource : public IGraphicsResource
 {
 private:
 
-    uint8_t radius;
-    uint8_t a;
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-
     SDL_Surface* circleSurface;
-
+    uint8_t radius;
 public:
 
     CircleFillGraphicsResource(int id, std::string resourceName, float radius, uint8_t a, uint8_t r, uint8_t g, uint8_t b) : IGraphicsResource(id, resourceName)
     {
-        this->radius = (uint8_t) floor(radius);
-        this->a = a;
-        this->r = r;
-        this->g = g;
-        this->b = b;
+        this->radius = static_cast<uint8_t>(floor(radius));
 
-        circleSurface = SDL_CreateRGBSurface(0, 2*this->radius, 2*this->radius, 32, 0, 0, 0, 0);
-        uint32_t color = SDL_MapRGBA(this->circleSurface->format, this->r, this->g, this->b, this->a);
+        circleSurface = SDL_CreateRGBSurface(0, 2 * this->radius, 2 * this->radius, 32, 0, 0, 0, 0);
+        uint32_t color = SDL_MapRGBA(this->circleSurface->format, r, g, b, a);
         GraphicsUtility::FillCircle(circleSurface, this->radius, color);
     }
 
@@ -46,27 +36,27 @@ public:
 
     virtual void Render(SDL_Renderer* sdlRenderer, TransformComponent* transformComponent)
     {
-        if (this->circleSurface == NULL) {
+        if (this->circleSurface == nullptr) {
             // TODO: how do error/warnings? be nice to be able to print them somewhere, std::cout though lol
             return;
         }
 
-        if (transformComponent == NULL) {
+        if (transformComponent == nullptr) {
             return;
         }
 
-        if (sdlRenderer == NULL) {
+        if (sdlRenderer == nullptr) {
             return; 
         }
 
         SDL_Texture* circleTexture = SDL_CreateTextureFromSurface(sdlRenderer, this->circleSurface);
 
-        SDL_Rect dstRect = {(int)transformComponent->position.x - this->radius,
-                            (int)transformComponent->position.y - this->radius,
-                            2*this->radius,
-                            2*this->radius};
+        uint32_t w = static_cast<int>(transformComponent->position.x) - this->radius;
+        uint32_t h = static_cast<int>(transformComponent->position.y) - this->radius;
 
-        SDL_RenderCopy(sdlRenderer, circleTexture, NULL, &dstRect);
+        SDL_Rect dstRect = {w, h, 2*this->radius, 2*this->radius};
+
+        SDL_RenderCopy(sdlRenderer, circleTexture, nullptr, &dstRect);
     }
 };
 
