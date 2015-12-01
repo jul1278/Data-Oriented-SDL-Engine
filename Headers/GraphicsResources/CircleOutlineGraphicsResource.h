c3348e702690be8daaf4d09a3888323e8472d1a4
Julian <jul1278@gmail.com>
2015-12-01 17:41:29 +1000

    resharper fixes

--- Headers/GraphicsResources/CircleOutlineGraphicsResource.h
+++ Headers/GraphicsResources/CircleOutlineGraphicsResource.h
@@ -15,10 +15,6 @@ private:
 
     uint8_t radius;
     uint8_t thickness;
-    uint8_t a;
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
 
     SDL_Surface* circleSurface;
 
@@ -26,22 +22,17 @@ public:
 
     CircleOutlineGraphicsResource(int id, std::string resourceName, float radius, float thickness, uint8_t a, uint8_t r, uint8_t g, uint8_t b) : IGraphicsResource(id, resourceName)
     {
-        this->radius = (uint8_t) floor(radius);
+        this->radius = static_cast<uint8_t>(floor(radius));
 
-        if (thickness < radius && thickness > 0.0f) {
-            this->thickness = (uint8_t) floor(thickness);
+        if ((thickness < radius) && (thickness > 0.0f)) {
+            this->thickness = static_cast<uint8_t>(floor(thickness));
         } else {
             this->thickness = 1;
         }
 
-        this->a = a;
-        this->r = r;
-        this->g = g;
-        this->b = b;
-
         circleSurface = SDL_CreateRGBSurface(0, 2*this->radius, 2*this->radius, 32, 0, 0, 0, 0);
 
-        uint32_t color = SDL_MapRGBA(this->circleSurface->format, this->r, this->g, this->b, this->a);
+        uint32_t color = SDL_MapRGBA(this->circleSurface->format, r, g, b, a);
         GraphicsUtility::OutlineCircle(circleSurface, this->radius, this->thickness, color);
 
     };
@@ -55,27 +46,27 @@ public:
 
     virtual void Render(SDL_Renderer* sdlRenderer, TransformComponent* transformComponent)
     {
-        if (this->circleSurface == NULL) {
+        if (this->circleSurface == nullptr) {
             // TODO: how do error/warnings? be nice to be able to print them somewhere, std::cout though lol
             return;
         }
 
-        if (transformComponent == NULL) {
+        if (transformComponent == nullptr) {
             return;
         }
 
-        if (sdlRenderer == NULL) {
+        if (sdlRenderer == nullptr) {
             return;
         }
 
         SDL_Texture* circleTexture = SDL_CreateTextureFromSurface(sdlRenderer, this->circleSurface);
 
-        SDL_Rect dstRect = {(int)transformComponent->position.x - this->radius,
-                            (int)transformComponent->position.y - this->radius,
-                            2*this->radius,
-                            2*this->radius};
+        uint8_t h = static_cast<int>(transformComponent->position.y) - this->radius;
+        uint8_t w = static_cast<int>(transformComponent->position.x) - this->radius;
 
-        SDL_RenderCopy(sdlRenderer, circleTexture, NULL, &dstRect);
+        SDL_Rect dstRect = {w, h, 2*this->radius, 2*this->radius};
+
+        SDL_RenderCopy(sdlRenderer, circleTexture, nullptr, &dstRect);
     }
 };
 
