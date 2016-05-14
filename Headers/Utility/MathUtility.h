de30a6b7108fef1bb8eac4042191ab8a4abe4f21
Raman <justzeddicus@gmail.com>
2016-05-14 22:38:59 +0200

    Update MathUtility.h

--- Headers/Utility/MathUtility.h
+++ Headers/Utility/MathUtility.h
@@ -62,6 +62,54 @@ namespace MathUtility
 		Vector2D v = Vector2D(vector.x*cosf(angle) + vector.y*sinf(angle), -vector.x*sinf(angle) + vector.y*cosf(angle)); 
 		return v; 
 	}
+	//---------------------------------------------------------------------------
+	// Name: RandInt
+	// Desc: returns a random integer from an interval
+	//---------------------------------------------------------------------------
+	static int RandInt(int left, int right, int step = 1) 
+	{ 
+
+		if (left > right)
+			std::swap(left, right);
+	
+		if (step <= 0 || left == right || abs(left) + abs(right) < step)
+			return left;
+	
+		int aright = abs(right);
+		int aleft = abs(left);
+	
+		if (aleft > aright)
+			std::swap(aleft, aright);
+	
+		int r = 1;
+		int c = 0;
+	
+		if (right <= 0 || left >= 0) { // if the interval is fully negative or fully positive
+			left >= 0 ? r = 1 : r = -1;
+			r *= rand() % (aright - aleft) + aleft;
+		}
+		else {
+			aright += aleft;
+			aleft = 0;
+			r *= rand() % (aright - aleft) - abs(left);
+			c = abs(left);
+		}
+	
+		r += c;
+	
+		if (rand() % 2) { // round down
+			r = (r / step) * step;
+			r = std::max(r, left);
+		}
+		else { // round up
+			r = (r / step + 1) * step;
+			r = std::min(r, right + c);
+		}
+	
+		r -= c;
+	
+		return r;
+	}
 };
 
 #endif // MATH_UTILITY_H
