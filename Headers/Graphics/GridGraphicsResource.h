1b18590534364bd4112752b98619cbe5aae9673a
Julian P <jul1278@gmail.com>
2016-05-21 09:31:14 +1000

    added GridGraphicsResource for drawing regular grids

---
+++ Headers/Graphics/GridGraphicsResource.h
@@ -0,0 +1,58 @@
+#ifndef GRID_GRAPHICS_RESOURCE_H
+#define GRID_GRAPHICS_RESOURCE_H
+
+#include "Graphics/IGraphicsResource.h"
+#include "SDL.h"
+
+class GridGraphicsResource : public IGraphicsResource
+{
+private:
+
+	float interval; 
+	unsigned int xCount;
+	unsigned int yCount; 
+
+	char a;
+	char r;
+	char g;
+	char b; 
+
+public:
+
+	GridGraphicsResource(float interval, unsigned int xCount, unsigned int yCount, char a, char r, char g, char b)
+	{
+		this->interval = interval;
+		this->xCount = xCount;
+		this->yCount = yCount;
+
+		this->a = a;
+		this->r = r; 
+		this->g = g; 
+		this->b = b; 
+	}
+
+	void Render(SDL_Renderer* sdlRenderer, TransformComponent* transformComponent) override final
+	{
+		// NOTE: this doesn't take into account scaling or orientation! TODO: 
+
+		SDL_SetRenderDrawColor(sdlRenderer, this->r, this->g, this->b, this->a); 
+
+		for (auto x = 0; x < this->xCount; x++) {
+			auto xCoord = transformComponent->position.x + x*interval;
+			auto y1Coord = transformComponent->position.y; 
+			auto y2Coord = transformComponent->position.y + yCount*interval; 
+
+			SDL_RenderDrawLine(sdlRenderer, xCoord, y1Coord, xCoord, y2Coord); 
+		}
+
+		for (auto y = 0; y < this->yCount; y++) {
+			auto yCoord = transformComponent->position.y + y*interval; 
+			auto x1Coord = transformComponent->position.x; 
+			auto x2Coord = transformComponent->position.x + xCount*interval; 
+
+			SDL_RenderDrawLine(sdlRenderer, x1Coord, yCoord, x2Coord, yCoord); 
+		}
+	}
+};
+
+#endif // GRID_GRAPHICS_RESOURCE_H
