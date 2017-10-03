01b43c1c347dd8dfa55604cf8c59b01997f139d8
Julian Pattie <jul1278@gmail.com>
2017-10-03 18:01:45 +1000

    inital check in of spatial hash table and test

--- Headers/Utility/SpatialHashTable.h
+++ Headers/Utility/SpatialHashTable.h
@@ -3,13 +3,64 @@
 
 #include <vector>
 #include <list>
+#include <set>
 #include <algorithm>
+#include <memory>
 #include <assert.h>
 
+
+//-----------------------------------------------------------------------------------------------------------------------
+// Name: Quad
+// Desc:
+//-----------------------------------------------------------------------------------------------------------------------
+template<typename T>
+struct Quad {
+	
+	const T obj;
+	
+	unsigned int topLeftX; 
+	unsigned int topLeftY;
+
+	unsigned int width; 
+	unsigned int height; 
+
+	//-----------------------------------------------------------------------------------------------------------------------
+	// Name: Quad
+	// Desc:
+	//-----------------------------------------------------------------------------------------------------------------------
+	Quad(unsigned int topLeftX, unsigned int topLeftY, unsigned int width, unsigned int height, const T& setObj) :
+		obj(setObj), topLeftX(topLeftX), topLeftY(topLeftY), width(width), height(height) 
+	{
+	} 
+
+	//-----------------------------------------------------------------------------------------------------------------------
+	// Name: ContainsPoint
+	// Desc:
+	//-----------------------------------------------------------------------------------------------------------------------
+	bool ContainsPoint(unsigned int x, unsigned int y) 
+	{
+		if (x >= this->topLeftX && y >= this->topLeftY) {
+			
+			if (x <= (this->width + this->topLeftX)
+				&& y <= (this->height + this->topLeftY)) {
+				
+				return true; 
+			}
+		}
+
+		return false; 
+	}
+};
+
+//-----------------------------------------------------------------------------------------------------------------------
+// Name: SpatialHashTable
+// Desc:
+//-----------------------------------------------------------------------------------------------------------------------
 template<typename T>
 class SpatialHashTable 
 {
-	std::vector<std::list<T>> buckets; 
+	std::vector<std::list<std::shared_ptr<Quad<T>>>> buckets;
+
 
 	unsigned int width; 
 	unsigned int height; 
@@ -48,19 +99,21 @@ public:
 			return; 
 		}
 
-		auto xStart = topLeftX / this->bucketSizeX; 
-		auto xEnd = (topLeftX + width) / this->bucketSizeX; 
+		auto xStart = topLeftX / this->bucketSizeX;
+		auto xEnd = (topLeftX + width) / this->bucketSizeX;
 
 		auto yStart = topLeftY / this->bucketSizeY;
-		auto yEnd = (topLeftY + height) / this->bucketSizeY; 
+		auto yEnd = (topLeftY + height) / this->bucketSizeY;
+
+		auto quadPtr = std::shared_ptr<Quad<T>>(new Quad<T>(topLeftX, topLeftY, width, height, obj)); 
 
 		for (unsigned int y = yStart; y <= yEnd; y++) {
 			for (unsigned int x = xStart; x <= xEnd; x++) {
 
-				auto index = x + y * xBuckets; 
-
+				auto index = x + y * xBuckets;
 				if (index < buckets.size()) {
-					buckets[index].push_back(obj);
+
+					buckets[index].push_back(quadPtr);
 				}
 			}
 		}
@@ -70,13 +123,15 @@ public:
 	// Name: Query
 	// Desc:
 	//-----------------------------------------------------------------------------------------------------------------------
-	std::list<T> Query(unsigned int topLeftX, unsigned int topLeftY, unsigned int width, unsigned int height)
+	std::list<std::shared_ptr<Quad<T>>> Query(unsigned int topLeftX, unsigned int topLeftY, unsigned int width, unsigned int height)
 	{
-		std::list<T> query; 
+		std::list<std::shared_ptr<Quad<T>>> query;
 
 		if (topLeftX > this->width || topLeftY > this->height) {
-			return std::list<T>();
+			return query;
 		}
+
+		std::set<std::shared_ptr<Quad<T>>> querySet;
 
 		auto xStart = topLeftX / this->bucketSizeX;
 		auto xEnd = (topLeftX + width) / this->bucketSizeX;
@@ -90,9 +145,13 @@ public:
 				auto index = x + y * xBuckets;
 
 				if (index < buckets.size()) {
+					for (auto& c : buckets[index]) {
 
-					for (auto c : buckets[index]) {
-						query.push_back(c);
+						// is c in the set? if not insert it
+						if (querySet.find(c) == querySet.end()) {
+							query.push_back(c);
+							querySet.insert(c); 
+						}
 					}
 				}
 			}
@@ -102,12 +161,48 @@ public:
 	}
 
 	//-----------------------------------------------------------------------------------------------------------------------
+	// Name: Query
+	// Desc:
+	//-----------------------------------------------------------------------------------------------------------------------
+	std::list<std::shared_ptr<Quad<T>>> Query(unsigned int x, unsigned int y)
+	{
+		std::list<std::shared_ptr<Quad<T>>> query;
+
+		if (x > this->width || y > this->height) {
+			return query;
+		}
+
+		std::set<std::shared_ptr<Quad<T>>> querySet; 
+
+		auto xIndex = x / xBuckets; 
+		auto yIndex = y / yBuckets; 
+
+		auto index = xIndex + yIndex * xBuckets;
+
+		if (index < buckets.size()) {
+
+			for (auto& c : buckets[index]) {
+						
+				if (c->ContainsPoint(x, y)) {
+
+					if (querySet.find(c) == querySet.end()) {
+						query.push_back(c);
+						querySet.insert(c); 
+					}
+				}
+			}
+		}
+		
+		return query;
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------------
 	// Name: Clear
 	// Desc:
 	//-----------------------------------------------------------------------------------------------------------------------
 	void Clear()
 	{
-		std::for_each(this->buckets.front(), this->buckets->back(), [](std::list<T>& l) {l.clear(); }); 
+		std::for_each(this->buckets.front(), this->buckets->back(), [] (std::list<Quad<T>>& l) { l.clear(); });
 	}
 };
 
--- Win/SimpleComponentsTest/SpatialHashTest.cpp
+++ Win/SimpleComponentsTest/SpatialHashTest.cpp
@@ -4,10 +4,17 @@
 
 #include <algorithm>
 #include <list>
+#include <functional>
 #include <iostream>
 
+bool wasCalled = false; 
+
+void TestFunction()
+{
+	wasCalled = true;
+}
 //-------------------------------------------------------------------------
-// Name: MultipleCollections
+// Name: TestInsertAndQuery
 // Desc:
 //-------------------------------------------------------------------------
 TEST(SpatialHashTests, TestInsertAndQuery)
@@ -16,9 +23,48 @@ TEST(SpatialHashTests, TestInsertAndQuery)
 
 	spatialHashTable.Hash(15, 15, 10, 10, 25); 
 
+	// we've hashed into buckets (1, 1), (1, 2), (2,1), (2,2)
+
 	auto result1 = spatialHashTable.Query(20, 20, 1, 1); 
 	auto result2 = spatialHashTable.Query(26, 26, 1, 1);
+	auto result3 = spatialHashTable.Query(30, 22, 1, 1); 
+	auto result4 = spatialHashTable.Query(0, 0, 100, 100); 
 
 	EXPECT_EQ(result1.size(), 1);
-	EXPECT_EQ(result2.size(), 0);
+	EXPECT_EQ(result2.size(), 1);
+	EXPECT_EQ(result3.size(), 0);
+	EXPECT_EQ(result4.size(), 1);
+}
+//-------------------------------------------------------------------------
+// Name: TestInsertAndQueryFunction
+// Desc:
+//-------------------------------------------------------------------------
+TEST(SpatialHashTests, TestInsertAndQueryFunction)
+{
+	SpatialHashTable<const std::function<void(void)>> spatialHashTable(100, 100, 10, 10);
+	spatialHashTable.Hash(15, 15, 10, 10, TestFunction);
+
+	auto result1 = spatialHashTable.Query(20, 20, 1, 1);
+	
+	for (auto r : result1) {
+		r->obj(); 
+	}
+
+	EXPECT_TRUE(wasCalled); 
+	EXPECT_EQ(result1.size(), 1);
+}
+//-------------------------------------------------------------------------
+// Name: TestInsertAndQueryPoint
+// Desc:
+//-------------------------------------------------------------------------
+TEST(SpatialHashTests, TestInsertAndQueryPoint)
+{
+	SpatialHashTable<int> spatialHashTable(100, 100, 10, 10);
+
+	spatialHashTable.Hash(76, 34, 2, 2, 25);
+
+	auto result = spatialHashTable.Query(77, 34);
+	
+	ASSERT_EQ(result.size(), 1);
+	EXPECT_EQ(result.front()->obj, 25); 
 }
